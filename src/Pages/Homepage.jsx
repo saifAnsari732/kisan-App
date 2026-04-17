6aa68718b1ffa300c8c302f7d94288a4984a28d1
kisan choice <hrmangamentkishanchoice@gmail.com>
2026-04-17 17:49:10 +0530

    set

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -78,7 +78,7 @@ export default function HeroCarousel() {
             <img
               src={slides[current].img}
               alt="hero"
-              className=" h-full object-cover  md:object-cover object-center"
+              className=" h-full object-cove  md:object-cover object-center"
               
             />
           </motion.div>
